2f9eca548485166ccad6a0a03e3203e0a4608214
Luka Gaganidze <LukaGaganidzeDev@gmail.com>
2023-09-01 19:07:05 +0400

    resume_v2

--- src/sections/projects/componenets/Resume.js
+++ src/sections/projects/componenets/Resume.js
@@ -13,7 +13,7 @@ const Resume = () => {
     <div className={classes["resume"]}>
       <Link
         target="_blank"
-        href="https://drive.google.com/file/d/1d9_kR7nHxibXNL1oLRuBdbP8MFwWTjSD/view"
+        href="https://drive.google.com/file/d/1sRnPVa2RrAVY8gWWQmnkGaJD9uRghPBn/view?usp=drive_link"
         className={classes["resume__text"]}
       >
         <h3 className={headingFont.className}>CHECK OUT MY RÉSUMÉ</h3>
