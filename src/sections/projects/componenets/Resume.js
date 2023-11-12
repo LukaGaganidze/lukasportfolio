0a93b717bc415059d0c6686d555efbd3b2ac040d
Luka Gaganidze <LukaGaganidzeDev@gmail.com>
2023-11-12 15:30:08 +0400

    resume update

--- src/sections/projects/componenets/Resume.js
+++ src/sections/projects/componenets/Resume.js
@@ -13,7 +13,7 @@ const Resume = () => {
     <div className={classes["resume"]}>
       <Link
         target="_blank"
-        href="https://drive.google.com/file/d/1m2YW0jGuBsmOkMaE58dtMfgd8-_dZWQd/view"
+        href="https://drive.google.com/file/d/1gxDV6rvNDSRp3fLqIWtJajILGqqczDXl/view?usp=drive_link"
         className={classes["resume__text"]}>
         <h3 className={headingFont.className}>CHECK OUT MY RÉSUMÉ</h3>
       </Link>
